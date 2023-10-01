076b7fcb8a30c62e4422347999372376886f0664
William Niu <wniu721@gmail.com>
2023-09-30 22:36:49 -0400

    Initialize Sphinx documentation with AutoAPI.

---
+++ docs/source/conf.py
@@ -0,0 +1,25 @@
+project = 'Piscis'
+copyright = '2023, William Niu'
+author = 'William Niu'
+release = '0.0.1'
+
+extensions = [
+    'sphinx.ext.intersphinx',
+    'autoapi.extension',
+    'numpydoc'
+]
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'flax': ('https://flax.readthedocs.io/en/latest/', None),
+    'jax': ('https://jax.readthedocs.io/en/latest/', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'pandas': ('https://pandas.pydata.org/docs/', None),
+    'xarray': ('https://xarray.pydata.org/en/stable/', None),
+}
+autoapi_dirs = ['../../piscis']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'pydata_sphinx_theme'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,6 @@
+Welcome to Piscis's documentation!
+==================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
