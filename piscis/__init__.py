ed7626842aae115629165c2556be5a49d0182d9d
William Niu <wniu721@gmail.com>
2024-09-06 23:28:59 -0400

    Convert CACHE_DIR to str.

--- piscis/__init__.py
+++ piscis/__init__.py
@@ -9,7 +9,7 @@ def _initialize_jax_cache() -> None:
     from piscis.paths import CACHE_DIR
 
     # Set the compilation cache directory.
-    compilation_cache.set_cache_dir(CACHE_DIR)
+    compilation_cache.set_cache_dir(str(CACHE_DIR))
 
 
 _initialize_jax_cache()
